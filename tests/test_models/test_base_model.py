535a8177cd8ff2b31b50b3bdee1017ff7ac74c66
mstom332 <tomas@332.com.uy>
2021-06-30 18:56:43 +0000

    add

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,8 +1,40 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
 
 
 #================================================================================
-my_model = BaseModel()
-my_model.name = "Holberton"
-my_model.my_number = 89
+
+import unittest
+from models.base_model import BaseModel
+import pep8
+from datetime import datetime
+import inspect
+import json
+
+
+class Test_pep8(unittest.TestCase):
+    """pep8 test cases class"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style Werrors (and warnings).")
+
+class TestDocs(unittest.TestCase):
+    """Base model document tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Testing class"""
+        cls.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
+
+    def test_module_docstring(self):
+        """module docstring length"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """Class docstring length"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+if __name__ == "__main__":
+    unittest.main()
