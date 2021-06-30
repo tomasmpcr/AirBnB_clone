0775f542373f4126762d5899378068424fafc8a3
mstom332 <tomas@332.com.uy>
2021-06-30 18:39:34 +0000

    add test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+
+#================================================================================
+my_model = BaseModel()
+my_model.name = "Holberton"
+my_model.my_number = 89
